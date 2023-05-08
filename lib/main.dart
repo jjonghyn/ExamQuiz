f2eea9078c198ce56ba8476a5aea0fff0b658f91
jjonghyn <jjonghyunn0802@gmail.com>
2023-05-08 13:30:59 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,88 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+
+
+  void playSound(int number){
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red
+                ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                      child: Text('clickMe'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.orange
+                ),
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text('clickMe'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.yellow
+                ),
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text('clickMe'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green
+                ),
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text('clickMe'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.teal
+                ),
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text('clickMe'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.blue
+                ),
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text('clickMe'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.purple
+                ),
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text('clickMe'),
+              ),
+            ],
+          ),
         ),
       ),
     );
