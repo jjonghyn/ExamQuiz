e1d18e28d8d0cc97f86c00d78513996496391c84
jjonghyn <jjonghyunn0802@gmail.com>
2023-05-17 11:13:24 +0900

    first commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\clone_flutter\thirdExample"
+export "FLUTTER_APPLICATION_PATH=C:\clone_flutter\quiz"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
